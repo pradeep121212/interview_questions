51ee2ddef78a4809b8982cf7c532fd7420be1854
Pradeep Kumaran <48370216+pradeep121212@users.noreply.github.com>
2020-03-11 04:45:38 +0530

    Update question_2.java

--- question_2.java
+++ question_2.java
@@ -4,7 +4,8 @@ Interview Question #2:
 
 Construct an algorithm to check whether two words (or phrases) are anagrams or not!
 
-"An anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once"
+"An anagram is a word or phrase formed by rearranging the letters of a different word or phrase,
+typically using all the original letters exactly once"
 
 */
 
